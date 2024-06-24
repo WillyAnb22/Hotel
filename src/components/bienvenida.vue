82b74b52ee82f79e918691f25222e04980d7e6e9
WillyAnb22 <161175216+WillyAnb22@users.noreply.github.com>
2024-06-23 22:22:49 -0500

    Update bienvenida.vue

--- src/components/bienvenida.vue
+++ src/components/bienvenida.vue
@@ -1,49 +1,64 @@
 <template>
-    <div><br>
+    <div>
         <img class="imgIncio" src="../hotelinicio.jpg">
-
+        <div class="overlay"></div>
         <div class="tarjeta">
-            <div class="cuadro">
-                <div class="parrafo">
-                    Bienvenidos a [Nombre del Hotel]<br>
-
-                    Combinamos lujo y comodidad para ofrecerle una estancia inolvidable.<br>
-                    Nuestras elegantes habitaciones, servicios de primera clase y vistas unicas
-                    que le permitaran
-                    disfrutar de una buenas vacaciones, que te haran espacar de tu realidad.<br>
-
-                    Aproveche nuestras ofertas especiales y paquetes para vivir una experiencia
-                    única a precios inigualables.<br>
-                    Reserve ahora y comience a planear tu próxima escapada.<br>
-                    Reserve su habitación hoy y descubra por qué [Nombre del Hotel] es el destino preferido para
-                    descansar.<br>
-                    ¡Esperamos darle la bienvenida pronto!
-                </div>
+            <div class="parrafo">
+                <div class="bienvenido">Bienvenidos a Hotel</div>
+                <hr><br>
+                Combinamos lujo y comodidad para ofrecerle una estancia inolvidable.
+                Nuestras elegantes habitaciones, servicios de primera clase y vistas
+                unicas te permitaran disfrutar de unas vacaciones
+                que te haran escapar de tu realidad.<br><br>
+                ¡Esperamos darle la bienvenida pronto!
             </div>
         </div>
+        <q-btn push to="../contacto" label="CONTACTANOS" />
     </div>
 </template>
 <style>
 .imgIncio {
     position: absolute;
     bottom: 0%;
+    width: 100%;
 }
 
-.cuadro {
-    background-color: #f2f2f2;
-    padding: 20px;
-    border: 10px solid #f1a90e;
-    margin: 20px;
-    width: 700px;
-    height: 500px;
+.overlay {
     position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.5);
+    /* Ajusta el color y la opacidad según tus necesidades */
 }
-.parrafo{
-    font-size: 18px; /* Tamaño de fuente */
-            line-height: 1.6; /* Altura de línea */
-            text-align: justify; /* Justificar texto */
+
+hr {
+    border: 1px solid #fff;
+}
+
+.bienvenido {
+    font-size: 30px;
+}
+
+.parrafo {
+    color: white;
+    position: absolute;
+    font-size: 20px;
+    font-style: italic;
+    text-align: left;
+    bottom: 38%;
+    left: 2%;
+    max-width: 450px;
+    text-align: center;
+}
+
+.q-btn {
+    background-color: antiquewhite;
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: 0 0 10px black;
+    margin-top: 37%;
+    left: 85%;
 }
 </style>
